2dd52112c1d33dee6342035252d0c4a20faee249
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2026-08-03 14:23:49 +0900

    fix: task-categoryのresourceルートからshowを除外
    TaskCategoryControllerにshow()メソッドが存在しないにもかかわらず、
    Route::resourceがshowルートを登録していたため、GET /admin/task-category/{task_category}
    にアクセスするとBadMethodCallExceptionが発生する状態だった。
    兄弟実装のannouncement.phpに倣い->except(['show'])を追加して防止する。

--- routes/admin/task.php
+++ routes/admin/task.php
@@ -6,4 +6,4 @@ use App\Http\Controllers\Admin\TaskCategoryController;
 
 // 認証・権限ミドルウェアは admin.php 側の親グループで適用済みのためここでは付与しない
 
-Route::resource('task-category', TaskCategoryController::class)->parameters(['task-category' => 'task_category']);
+Route::resource('task-category', TaskCategoryController::class)->parameters(['task-category' => 'task_category'])->except(['show']);
